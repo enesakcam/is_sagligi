8478e61080106e16b0914f0e67b2dd53d0b75d8d
enes482 <enesakcm9@gmail.com>
2024-02-22 10:06:26 +0300

    Proje dosyası ekle.

---
+++ enes482/Controllers/listeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace enes482.Controllers
+{
+    public class listeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("liste/marka/{id}/{ad}")]
+        public string marka(int id, string ad)
+        {
+            return $"Parametreden gelen id={id}, ad={ad}";
+        }
+    }
+}
